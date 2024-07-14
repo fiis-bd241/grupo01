0bfc0e4ceda71455af9f4f8352e86d37bffe44bb
José Joaquín Alvarado Usucachi <joaquin14142@gmail.com>
2024-07-14 12:05:30 -0500

    Update Conexion.java

--- 08. Aplicación/sanfernando-back/src/main/java/com/sanfernando/sanfernando/utils/Conexion.java
+++ 08. Aplicación/sanfernando-back/src/main/java/com/sanfernando/sanfernando/utils/Conexion.java
@@ -18,7 +18,7 @@ public class Conexion {
 
   private Connection con;
 
-  String url = "jdbc:postgresql://localhost:5432/sf-masivo";
+  String url = "jdbc:postgresql://localhost:5432/san-fernando-db";
   String username = "postgres";
   String password = "123456";
   
